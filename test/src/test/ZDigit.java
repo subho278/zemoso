915595de61dce5ffa492eaa7e6dc48d162d89a94
zemoso02 <zemoso02@zemoso02-Vostro-3546.(none)>
2015-06-12 15:29:09 +0530

    ZAlgebra Assignments

---
+++ test/src/test/ZDigit.java
@@ -0,0 +1,48 @@
+package test;
+
+//import java.util.Scanner;
+
+public final class ZDigit {
+
+	private char character;
+
+	public ZDigit(char c) {
+		String s = Character.toString(c);
+		if (s.matches("[A-Z0]")) {
+			this.character = c;
+		} else {
+			throw new IllegalArgumentException("Entered value is not a ZDigit");
+		}
+	}
+
+	public char getZDigit() {
+		return character;
+	}
+
+	public int getValue() {
+
+		String base = new String();
+		base = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		int value = base.indexOf(character);
+		return value;
+
+	}
+
+	public String toString() {
+		// return ""+character;
+		// return String.valueOf(character);
+		return Character.toString(character);
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * Scanner in = new Scanner(System.in);
+		 * System.out.print("Enter a ZDigit: "); char c =
+		 * in.next(".").charAt(0);
+		 */
+		ZDigit zd = new ZDigit('A');
+		System.out.println("ZDigit is " + zd);
+		System.out.println("Value of ZDigit is " + zd.getValue());
+	}
+}
---
+++ test/src/test/ZNumber.java
@@ -0,0 +1,90 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//import java.util.Scanner;
+public final class ZNumber {
+
+	private String zStr;
+
+	public ZNumber(char[] digits) {
+		StringBuilder sb = new StringBuilder();
+		for (char value : digits) {
+			ZDigit zd = new ZDigit(value);
+			sb.append(zd);
+		}
+		this.zStr = sb.toString();
+	}
+
+	public ZNumber(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			ZDigit zd = new ZDigit(s.charAt(i));
+			sb.append(zd);
+		}
+		this.zStr = sb.toString();
+	}
+
+	public ZNumber(ArrayList<Character> li) {
+		StringBuilder sb = new StringBuilder();
+		for (Character value : li) {
+			ZDigit zd = new ZDigit(value);
+			sb.append(zd);
+		}
+		this.zStr = sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		return zStr;
+	}
+
+	public String toDecimal() {
+
+		int value = 0;
+		ZDigit[] digits = getDigits();
+//		for (int i = 0; i < digits.length; i++) {
+		for(ZDigit digit: digits){
+//		ZDigit digit = digits[i];
+			value = 27 * value + digit.getValue();
+		}
+		this.zStr = Integer.toString(value);
+		return zStr;
+	}
+
+	public ZDigit[] getDigits() {
+		ZDigit[] zd = new ZDigit[zStr.length()];
+		for (int i = 0; i < zStr.length(); i++) {
+			zd[i] = new ZDigit(zStr.charAt(i));
+		}
+		return zd;
+	}
+
+	public static void main(String[] args) {
+		ZNumber zn = new ZNumber(new char[] { 'A', 'B', 'C' });
+		// zn = new ZNumber("ABC".toCharArray());
+		System.out.println(zn);
+		zn.toDecimal();
+		System.out.println("Decimal value of ZNumber is " + zn.zStr);
+
+		ZNumber zn1 = new ZNumber("ABCDE");
+		System.out.println(zn1);
+
+		// ArrayList<Character> li = new ArrayList<Character>();
+		ArrayList<Character> li = new ArrayList<Character>(Arrays.asList('D','E'));
+		// li.add('D');
+		// li.add('E');
+		System.out.println(li);
+
+		ZNumber zn2 = new ZNumber(li);
+		System.out.println(zn2);
+
+		ZDigit[] zd = zn2.getDigits();
+		System.out.print("Zdigits are ");
+		for (ZDigit digit : zd) {
+			System.out.print(digit.getZDigit() + " ");
+		}
+
+	}
+}
